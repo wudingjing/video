d03af2aa015bacb376f26a1ba2829f65e75ad24d
TomCat <657475779@qq.com>
2020-10-19 20:42:23 +0800

    删除*

--- video/video-service/src/main/java/com/qf/service/CourseService.java
+++ video/video-service/src/main/java/com/qf/service/CourseService.java
@@ -1,4 +1,10 @@
 package com.qf.service;
 
+import com.qf.pojo.Course;
+
+import java.util.List;
+
 public interface CourseService {
+    List<Course> findAll();
+
 }
--- video/video-service/src/main/java/com/qf/service/impl/CourseServiceImpl.java
+++ video/video-service/src/main/java/com/qf/service/impl/CourseServiceImpl.java
@@ -1,8 +1,21 @@
 package com.qf.service.impl;
 
+import com.qf.dao.CourseMapper;
+import com.qf.pojo.Course;
 import com.qf.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseMapper courseMapper;
+
+    @Override
+    public List<Course> findAll() {
+        return courseMapper.selectByExample(null);
+    }
 }
