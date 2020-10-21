384a037eeca83da980c81b2722b7420323572bef
TomCat <657475779@qq.com>
2020-10-21 14:53:59 +0800

    最终版本2.0

--- video/video-pojo/src/main/java/com/qf/pojo/User.java
+++ video/video-pojo/src/main/java/com/qf/pojo/User.java
@@ -1,13 +1,16 @@
 package com.qf.pojo;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class User {
     private Integer id;
 
     private String email;
 
-    private String phonenum;
+    private String phoneNum;
 
     private String password;
 
@@ -17,99 +20,12 @@ public class User {
 
     private String sex;
 
-    private String birthday;
+    private String birthDay;
 
     private String address;
 
     private String imgUrl;
 
-    private Date createtime;
+    private Date createTime;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email == null ? null : email.trim();
-    }
-
-    public String getPhonenum() {
-        return phonenum;
-    }
-
-    public void setPhonenum(String phonenum) {
-        this.phonenum = phonenum == null ? null : phonenum.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code == null ? null : code.trim();
-    }
-
-    public String getnickName() {
-        return nickName;
-    }
-
-    public void setnickName(String nickName) {
-        this.nickName = nickName == null ? null : nickName.trim();
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex == null ? null : sex.trim();
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday == null ? null : birthday.trim();
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl == null ? null : imgUrl.trim();
-    }
-
-    public Date getCreatetime() {
-        return createtime;
-    }
-
-    public void setCreatetime(Date createtime) {
-        this.createtime = createtime;
-    }
 }
